3fac6eb4f9b1b93ff1c75d33aa17385e96c45f5f
Alberto Zarraga <zarraga555@gmail.com>
2025-04-21 01:45:50 -0400

    feat(user-management): add routes for user management
    Introduce new routes for user management, including index, create, and edit functionalities. This change enables the application to handle user-related operations more effectively.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\UserManagement\Users\Index;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -17,6 +18,12 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+
+    Route::prefix('user-management')->group(function () {
+        Volt::route('users','user-management.users.index')->name('user-management.users.index');
+        Volt::route('users/create','user-management.users.create')->name('user-management.users.create');
+        Volt::route('users/{user}/edit','user-management.users.edit')->name('user-management.users.edit');
+    });
 });
 
 require __DIR__.'/auth.php';
