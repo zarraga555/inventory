2fb4142bc27a0fecb1f27a690ece54acf77a5bce
Alberto Zarraga <zarraga555@gmail.com>
2025-04-21 01:50:26 -0400

    feat(user-management): add users index view for user management
    Introduce a new Blade template for the users index view in the user management section. This view includes a heading, subheading, and a button to create new users, along with a table to display existing users. This is part of the user management feature to allow administrators to manage users effectively.

---
+++ resources/views/livewire/user-management/users/index.blade.php
@@ -0,0 +1,20 @@
+@section('title')
+    {{ __('Users') }}
+@endsection
+<div>
+    <div class="relative mb-6 w-full">
+        <div class="flex justify-between items-center">
+            <div>
+                <flux:heading size="xl" level="1">{{ __('Users') }}</flux::heading>
+                    <flux:subheading size="lg">{{ __('Manage your users') }}</flux::subheading>
+            </div>
+            <flux:button href="{{ route('user-management.users.create') }}" variant="primary" wire:navigate>
+                {{ __('Create new user') }}
+            </flux:button>
+        </div>
+    </div>
+    <flux:separator variant="subtitle" />
+    <div class="mt-8 self-stretch max-md:pt-6">
+        @include('livewire.user-management.users.table')
+    </div>
+</div>
