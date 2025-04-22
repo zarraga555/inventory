20d9e7eb34462d55b8e6f923f93b368a1c547cf8
Alberto Zarraga <zarraga555@gmail.com>
2025-04-22 16:09:04 -0400

    refactor(views): improve readability and add wire:click handlers
    Restructure the breadcrumbs for better readability and add wire:click handlers to buttons for save and save and create another actions

--- resources/views/livewire/user-management/users/create.blade.php
+++ resources/views/livewire/user-management/users/create.blade.php
@@ -7,7 +7,8 @@
             <div>
                 <flux:heading class="mb-2" size="xl" level="1">{{ __('Create new user') }}</flux::heading>
                     <flux:breadcrumbs>
-                        <flux:breadcrumbs.item href="{{ route('user-management.users.index') }}" wire:navigate>{{ __('Users') }}
+                        <flux:breadcrumbs.item href="{{ route('user-management.users.index') }}" wire:navigate>
+                            {{ __('Users') }}
                         </flux:breadcrumbs.item>
                         <flux:breadcrumbs.item>{{ __('Create') }}</flux:breadcrumbs.item>
                     </flux:breadcrumbs>
@@ -21,8 +22,8 @@
     </div>
 
     <div class="fi-ac gap-3 flex flex-wrap items-center justify-start mt-8">
-        <flux:button variant="primary">{{__('Save')}}</flux:button>
-        <flux:button>{{__('Save and create another')}}</flux:button>
-        <flux:button href="{{ route('user-management.users.index') }}" wire:navigate>{{__('Cancel')}}</flux:button>
+        <flux:button variant="primary" wire:click="save">{{ __('Save') }}</flux:button>
+        <flux:button wire:click="saveAndCreateAnother">{{ __('Save and create another') }}</flux:button>
+        <flux:button href="{{ route('user-management.users.index') }}" wire:navigate>{{ __('Cancel') }}</flux:button>
     </div>
 </div>
