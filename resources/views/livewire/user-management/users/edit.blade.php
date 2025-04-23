65857dac39f0f3edfa6798bbbd991bbc302c9a22
Alberto Zarraga <zarraga555@gmail.com>
2025-04-23 01:06:20 -0400

    feat(user-management): add delete user modal and update save button
    Introduce a modal for confirming user deletion and update the save button to trigger the update method. This enhances the user management interface by providing a clear way to delete users and ensuring the save functionality is properly wired.

--- resources/views/livewire/user-management/users/edit.blade.php
+++ resources/views/livewire/user-management/users/edit.blade.php
@@ -7,21 +7,26 @@
             <div>
                 <flux:heading class="mb-2" size="xl" level="1">{{ __('Edit user') }}</flux::heading>
                     <flux:breadcrumbs>
-                        <flux:breadcrumbs.item href="{{ route('user-management.users.index') }}" wire:navigate>{{ __('Users') }}
+                        <flux:breadcrumbs.item href="{{ route('user-management.users.index') }}" wire:navigate>
+                            {{ __('Users') }}
                         </flux:breadcrumbs.item>
                         <flux:breadcrumbs.item>{{ __('Edit') }}</flux:breadcrumbs.item>
                     </flux:breadcrumbs>
             </div>
+            <flux:modal.trigger name="confirmingUserDeletion">
+                <flux:button variant="danger">{{__('Delete')}}</flux:button>
+            </flux:modal.trigger>
         </div>
     </div>
     <flux:separator variant="subtitle" />
 
     <div class="mt-8 self-stretch max-md:pt-6">
         @include('livewire.user-management.users.form')
+        @include('livewire.user-management.users.modalDelete')
     </div>
 
     <div class="fi-ac gap-3 flex flex-wrap items-center justify-start mt-8">
-        <flux:button variant="primary">{{__('Save changes')}}</flux:button>
-        <flux:button href="{{ route('user-management.users.index') }}" wire:navigate>{{__('Cancel')}}</flux:button>
+        <flux:button variant="primary" wire:click="update">{{ __('Save changes') }}</flux:button>
+        <flux:button href="{{ route('user-management.users.index') }}" wire:navigate>{{ __('Cancel') }}</flux:button>
     </div>
 </div>
