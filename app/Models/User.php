fa60dac4af4cbacb04b6546c80c104225c32f9d7
Alberto Zarraga <zarraga555@gmail.com>
2025-04-23 01:04:54 -0400

    refactor(User): add SoftDeletes trait to User model
    Enable soft deletion functionality for the User model to allow for non-destructive deletion of user records. This change ensures that users can be "deleted" while retaining their data in the database for potential recovery or auditing purposes.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
