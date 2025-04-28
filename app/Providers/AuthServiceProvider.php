6321d6d83c3e71b5a0bbeaf3bd9910b73db53939
Alberto Zarraga <zarraga555@gmail.com>
2025-04-28 18:09:13 -0400

    feat(auth): add AuthServiceProvider to define user policies
    Introduce AuthServiceProvider to register authentication and authorization services. This includes mapping the User model to the UserPolicy class to enforce user-related policies.

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use App\Policies\UserPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * El arreglo de políticas para la aplicación.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        User::class => UserPolicy::class,
+    ];
+
+    /**
+     * Registrar cualquier servicio de autenticación / autorización.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
