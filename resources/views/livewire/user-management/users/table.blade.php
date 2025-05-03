44b4a6dd2a70a35698a373700dd4d935a9ffcf56
Alberto Zarraga <zarraga555@gmail.com>
2025-05-03 18:34:28 -0400

    feat(user-management): add search functionality and improve table structure
    Introduce a search component for user management table and replace raw HTML table cells with `x-flux.table.cell` components for better consistency and maintainability

--- resources/views/livewire/user-management/users/table.blade.php
+++ resources/views/livewire/user-management/users/table.blade.php
@@ -1,3 +1,7 @@
+<x-flux.table.search 
+    model="search" 
+    placeholder="Search for users..." 
+/>
 <x-flux.table>
     <x-flux.table.columns>
         <x-flux.table.column>{{ __('Name') }}</x-flux.table.column>
@@ -6,35 +10,39 @@
         @if (auth()->user()?->can('users.logs.view') || auth()->user()?->can('users.update'))
             <x-flux.table.column>{{ __('Actions') }}</x-flux.table.column>
         @endif
-
     </x-flux.table.columns>
 
     <x-flux.table.rows>
         @forelse ($users as $user)
             <x-flux.table.row>
-                <td class="px-6 py-4">{{ $user->name }}</td>
-                <td class="px-6 py-4">{{ $user->email }}</td>
-                <td class="px-6 py-4">
-                    {{ isset($user->roles->pluck('name')[0]) ? $user->roles->pluck('name')[0] : __('No role') }}
-                </td>
+                <x-flux.table.cell>{{ $user->name }}</x-flux.table.cell>
+                <x-flux.table.cell>{{ $user->email }}</x-flux.table.cell>
+                <x-flux.table.cell>
+                    {{ $user->roles->pluck('name')->first() ?? __('No role') }}
+                </x-flux.table.cell>
+
                 @if (auth()->user()?->can('users.logs.view') || auth()->user()?->can('users.update'))
-                    <td class="px-6 py-4">
+                    <x-flux.table.cell>
                         @can('users.update')
                             <a href="{{ route('user-management.users.edit', $user->id) }}"
                                 class="text-blue-500 hover:underline">{{ __('Edit') }}</a>
                         @endcan
+
                         @can('users.logs.view')
-                            | <a href="{{ route('user-management.users.logs.view', $user->id) }}"
+                            @can('users.update')
+                                |
+                            @endcan
+                            <a href="{{ route('user-management.users.logs.view', $user->id) }}"
                                 class="text-primary-500 hover:underline">{{ __('See activity') }}</a>
                         @endcan
-                    </td>
+                    </x-flux.table.cell>
                 @endif
-
             </x-flux.table.row>
         @empty
             <x-flux.table.row>
-                <td class="px-6 py-4" colspan="4">
-                    {{ __('No records were found. Please create a record to get started.') }}</td>
+                <x-flux.table.cell colspan="4">
+                    {{ __('No records were found. Please create a record to get started.') }}
+                </x-flux.table.cell>
             </x-flux.table.row>
         @endforelse
     </x-flux.table.rows>
