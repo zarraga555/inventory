03792637d00bce335a614db4d00838d502cf1dbe
Alberto Zarraga <zarraga555@gmail.com>
2025-05-03 18:43:08 -0400

    refactor: remove commented-out code in column.blade.php
    Clean up the view component by removing unused and commented-out code to improve readability and maintainability

--- resources/views/components/flux/table/column.blade.php
+++ resources/views/components/flux/table/column.blade.php
@@ -1,16 +1,3 @@
-{{-- <th
-    {{ $attributes->merge(['class' => 'px-6 py-3 text-left font-medium cursor-pointer select-none hover:text-primary-600']) }}
->
-    {{ $slot }}
-
-    @if (isset($sortField) && $sortField === $field)
-        @if ($sortDirection === 'asc')
-            ▲
-        @else
-            ▼
-        @endif
-    @endif
-</th> --}}
 <th scope="col" class="py-3 px-3 text-start text-sm font-medium text-zinc-800 dark:text-white uppercase" data-flux-column="">
     <div class="flex">
         {{ $slot }}
