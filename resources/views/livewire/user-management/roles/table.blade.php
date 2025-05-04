8b56ff85483172a6fbc2b3c9c90097dae160c3fc
Alberto Zarraga <zarraga555@gmail.com>
2025-05-04 12:31:16 -0400

    feat(user-management): add roles table view for user management
    Introduce a new Livewire Blade component to display roles in a table format. The table includes search, sorting, and pagination functionality, along with actions for editing roles if the user has the necessary permissions. This enhances the user management interface by providing a structured way to view and manage roles.

---
+++ resources/views/livewire/user-management/roles/table.blade.php
@@ -0,0 +1,40 @@
+<x-flux.table.search model="search" placeholder="Search for roles..." />
+<x-flux.table>
+    <x-flux.table.columns>
+        <x-flux.table.sortable-column field="name" label="Name" :sortField="$sortField" :sortDirection="$sortDirection" />
+        <x-flux.table.column>{{ __('Permissions') }}</x-flux.table.column>
+        @if (auth()->user()?->can('roles.update'))
+            <x-flux.table.column>{{ __('Actions') }}</x-flux.table.column>
+        @endif
+    </x-flux.table.columns>
+
+    <x-flux.table.rows>
+        @forelse ($roles as $role)
+            <x-flux.table.row>
+                <x-flux.table.cell>{{ $role->name }}</x-flux.table.cell>
+                <x-flux.table.cell>
+                    @foreach ($role->permissions as $permission)
+                        <flux:badge size="sm">{{ __("{$permission->name}") }}</flux:badge>
+                    @endforeach
+                </x-flux.table.cell>
+
+                @if (auth()->user()?->can('roles.update'))
+                    <x-flux.table.cell>
+                        @can('roles.update')
+                            <a href="{{ route('user-management.roles.edit', $role->id) }}"
+                                class="text-blue-500 hover:underline">{{ __('Edit') }}</a>
+                        @endcan
+                    </x-flux.table.cell>
+                @endif
+            </x-flux.table.row>
+        @empty
+            <x-flux.table.row>
+                <x-flux.table.cell colspan="4">
+                    {{ __('No records were found. Please create a record to get started.') }}
+                </x-flux.table.cell>
+            </x-flux.table.row>
+        @endforelse
+    </x-flux.table.rows>
+</x-flux.table>
+
+<x-flux.table.pagination :paginator="$roles" />
