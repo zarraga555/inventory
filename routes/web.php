bf5fdbec9b97b5915f4087569739e7b67e513ee9
Alberto Zarraga <zarraga555@gmail.com>
2025-05-05 17:39:48 -0400

    style(routes): format spacing in route definitions
    Consistently apply spacing around commas and operators in route definitions to improve readability and maintain code style standards.

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,21 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
     Route::prefix('user-management')->group(function () {
-        Volt::route('users','user-management.users.index')->name('user-management.users.index')->middleware('can:users.view');
-        Volt::route('users/create','user-management.users.create')->name('user-management.users.create')->middleware('can:users.create');
-        Volt::route('users/{user}/edit','user-management.users.edit')->name('user-management.users.edit')->middleware('can:users.update');
-        Volt::route('users/{user}/logs','user-management.logs.index')->name('user-management.users.logs.view')->middleware('can:users.logs.view');
+        Volt::route('users', 'user-management.users.index')->name('user-management.users.index')->middleware('can:users.view');
+        Volt::route('users/create', 'user-management.users.create')->name('user-management.users.create')->middleware('can:users.create');
+        Volt::route('users/{user}/edit', 'user-management.users.edit')->name('user-management.users.edit')->middleware('can:users.update');
+        Volt::route('users/{user}/logs', 'user-management.logs.index')->name('user-management.users.logs.view')->middleware('can:users.logs.view');
     });
 
     Route::prefix('user-management')->group(function () {
-        Volt::route('roles','user-management.roles.index')->name('user-management.roles.index')->middleware('can:roles.view');
-        Volt::route('roles/create','user-management.roles.create')->name('user-management.roles.create')->middleware('can:roles.create');
-        Volt::route('roles/{role}/edit','user-management.roles.edit')->name('user-management.roles.edit')->middleware('can:roles.update');
+        Volt::route('roles', 'user-management.roles.index')->name('user-management.roles.index')->middleware('can:roles.view');
+        Volt::route('roles/create', 'user-management.roles.create')->name('user-management.roles.create')->middleware('can:roles.create');
+        Volt::route('roles/{role}/edit', 'user-management.roles.edit')->name('user-management.roles.edit')->middleware('can:roles.update');
     });
 });
 
-require __DIR__.'/auth.php';
+// Route::prefix('wizard')->group(function () {
+//     Volt::route('form', 'wizard.form-wizard')->name('wizzard.form');
+// });
+
+require __DIR__ . '/auth.php';
